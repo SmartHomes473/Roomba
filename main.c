161450a885411e20e902d78a08cdc8c2c8be9da1
unknown <rwooster@eecs373-04.adsroot.itcs.umich.edu>
2014-11-16 18:59:54 -0500

    adding device registration

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "msp430g2553.h"
 #include "msp430g2553_utils.h"
 #include "roomba.h"
+#include "string.h"
 #include "software_uart.h"
 #include "hardware_uart.h"
 #include "serialization.h"
 
 #define PACKET_START_BYTE 0x0F
 #define PACKET_END_BYTE 0x04
+#define DEVICE_URL "http://rwooster.com/roomba.tar.gz"
 
 typedef enum
 {
@@ -25,14 +27,40 @@ int main(void) {
 	softwareUART_init();
 	initialize_roomba();
 
+	/*
+
+	int* device_num = (int*)FLASH_START_ADDR;
+	flash_erase(device_num);
+	flash_write(device_num, 100);
+	int test_value = *device_num;
+
+	*/
+
 	uint8_t new_UART_RX;
 	parse_state_t state = NONE;
 	uint16_t packet_size = 1;
 	uint8_t data[10];
 	uint8_t data_idx = 0;
 
+	uint8_t* device_num_addr = (uint8_t*)FLASH_START_ADDR;
+	uint8_t device_num;
+	uint8_t status;
+
 	while (1)
 	{
+		/* Uninitialized flash memory is all 1 */
+		if (*device_num_addr == 255)
+		{
+			/* Register device */
+			uint8_t register_array[39] = {PACKET_START_BYTE, 0, 0, 0, 33};
+			memcpy(register_array+5, DEVICE_URL, 33);
+			register_array[38] = PACKET_END_BYTE;
+
+			UART_send_array(register_array, 39);
+
+			flash_erase(device_num_addr);
+			flash_write(device_num_addr, 0);
+		}
 
 		if (UART_data_available() == 1) {
 			new_UART_RX = UART_get_data();
@@ -44,9 +72,11 @@ int main(void) {
 				}
 				break;
 			case DEVICE_NUM:
+				device_num = new_UART_RX;
 				state = STATUS;
 				break;
 			case STATUS:
+				status = new_UART_RX;
 				state = SIZE;
 				break;
 			case SIZE:
@@ -67,7 +97,13 @@ int main(void) {
 			}
 		}
 
-		if (data_idx >= packet_size && state == DATA) {
+		if (status == 0 && packet_size == 0 && device_num != 0)
+		{
+			flash_erase(device_num_addr);
+			flash_write(device_num_addr, device_num);
+		}
+		else if (data_idx >= packet_size && state == DATA)
+		{
 			deserialize(data);
 			data_idx = 0;
 			state = END;
--- msp430g2553_utils.c
+++ msp430g2553_utils.c
@@ -17,6 +17,44 @@ void processor_init()
 	P1DIR |= BIT0;
 }
 
+/*************************************************************************
+ * Erase Information Flash
+ *************************************************************************/
+void flash_erase(uint8_t* addr)
+{
+	__disable_interrupt();               // Disable interrupts to make the erase atomic
+	while(BUSY & FCTL3);                 // Check if Flash being used
+	FCTL2 = FWKEY + FSSEL_1 + FN3;       // Clk = SMCLK/4
+	FCTL1 = FWKEY + ERASE;               // Set Erase bit
+	FCTL3 = FWKEY;                       // Clear Lock bit
+
+	*addr = 0;                           // Dummy write to erase Flash segment
+
+	while(BUSY & FCTL3);                 // Check if Flash being used
+	FCTL1 = FWKEY;                       // Clear WRT bit
+	FCTL3 = FWKEY + LOCK;                // Set LOCK bit
+	__enable_interrupt();
+}
+
+
+/*************************************************************************
+ * Read stored Information flash
+ *************************************************************************/
+void flash_write(uint8_t* addr, uint8_t byte)
+{
+	__disable_interrupt();
+	FCTL2 = FWKEY + FSSEL_1 + FN0;       // Clk = SMCLK/4
+	FCTL3 = FWKEY;                       // Clear Lock bit
+	FCTL1 = FWKEY + WRT;                 // Set WRT bit for write operation
+
+	*addr++ = byte;						// copy value to flash
+
+	FCTL1 = FWKEY;                        // Clear WRT bit
+	FCTL3 = FWKEY + LOCK;                 // Set LOCK bit
+	while(BUSY & FCTL3);
+	__enable_interrupt();
+}
+
 /******************************************************************************
 * Delay for XXXX Amount of time
 ******************************************************************************/
